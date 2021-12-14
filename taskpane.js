b81cf9585865c3fd5c1d4e9607ae402e1c1e0c5f
Gove Allen <gove.allen@gmail.com>
2021-12-14 07:34:56 -0700

    bring back .js

--- taskpane.js
+++ taskpane.js
@@ -1,23 +1,6 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global console, document, Excel, Office */
 
 console.log("starting--------------------------")
 
-
-
-
-Office.onReady((info) => {
-  console.log("at on ready")
-  if (info.host === Office.HostType.Excel) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
-});
 
 async function run() {
   try {
